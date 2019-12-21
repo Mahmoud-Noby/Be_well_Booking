1bf0b07c4e247b778e5de3b97fa8e6ebfc13ac2f
Mahmoud-Noby <mahmoudnoby146@outlook.com>
2019-12-21 13:29:03 +0200

    refactoring

--- js/doctor-details.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.client_preferred_time').datetimepicker({
-        datepicker:false,
-        formatTime:"h:i a",
-        step:30,
-        format:"h:i a"
-    });
-})
